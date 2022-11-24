1edcde0d6e4bbee7e0abec9dc6133a2374cd8c42
duykhanh06 <118660008+duykhanh06@users.noreply.github.com>
2022-11-24 16:43:54 +0700

    Add files via upload

---
+++ NHÓM 6_LAB4/Bài 1_1.sql
@@ -0,0 +1,10 @@
+﻿SELECT IIF(LUONG>=LTB,'KHÔNG TĂNG LƯƠNG','Tăng lương')
+as thuong,tennv,luong,ltb
+from
+
+(select TENNV,LUONG,ltb from NHANVIEN,
+(select avg(LUONG) as 'ltb',PHG from NHANVIEN group by PHG) AS TAM
+WHERE NHANVIEN.PHG = TAM.PHG) as abc
+
+SELECT * FROM NHANVIEN
+select avg(LUONG) as 'ltb',PHG from NHANVIEN group by PHG
