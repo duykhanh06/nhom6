dfff1513dd91d0a4fcf2d6c7b97e8f86efb4c3bb
duykhanh06 <118660008+duykhanh06@users.noreply.github.com>
2022-12-01 16:15:01 +0700

    Add files via upload

---
+++ NHOM6-LAB5/Câu 2B.sql
@@ -0,0 +1,17 @@
+select count(MANV), MADA, TENPHG from NHANVIEN
+inner join PHONGBAN on NHANVIEN.PHG = PHONGBAN.MAPHG
+inner join DEAN on DEAN.PHONG = PHONGBAN.MAPHG
+where MADA = 2
+group by TENPHG,MADA
+
+alter proc lab5_cau2_b @manv int
+as
+begin
+		select count(MANV) as 'so luong', MADA, TENPHG from NHANVIEN
+		inner join PHONGBAN on NHANVIEN.PHG = PHONGBAN.MAPHG
+		inner join DEAN on DEAN.PHONG = PHONGBAN.MAPHG
+		where MADA = @manv
+		group by TENPHG,MADA
+end
+exec lab5_cau2_b 10 
+go
